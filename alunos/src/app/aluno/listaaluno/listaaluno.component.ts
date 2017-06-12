a9194366a6659c06fdadb248f21ac27c4ce1dd13
GILSON RICARDO DE ALMEIDA <gilson.almeida@elotech.com.br>
2017-06-12 13:11:14 -0300

    lista alunos

--- alunos/src/app/aluno/listaaluno/listaaluno.component.ts
+++ alunos/src/app/aluno/listaaluno/listaaluno.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { SuperLista } from '../../comum/superlista.component';
+import { AlunoService } from './../../aluno.service';
 
 @Component({
-  selector: 'app-listaaluno',
-  templateUrl: './listaaluno.component.html',
-  styleUrls: ['./listaaluno.component.css']
+  selector: 'app-lista',
+  templateUrl: '../../comum/lista.component.html',
+  styleUrls:  ['../../comum/lista.component.css']
 })
-export class ListaalunoComponent implements OnInit {
+export class ListaalunoComponent extends SuperLista implements OnInit {
 
-  constructor() { }
+    constructor(service:AlunoService, router:Router, route: ActivatedRoute) {
+      super(service,router,route);
+      this.cols = [ {field: 'nome', header: 'Nomes'}, {field: 'version', header: 'Versão'} ];
+    
 
-  ngOnInit() {
-  }
+   }
+
 
 }
